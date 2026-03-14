908d07eb9319fbbbc56b8e5500a930711f183400
Harsh Baranwal <100343964+harshb2486@users.noreply.github.com>
2026-03-14 22:17:28 +0530

    added template for the frontend of the estore

---
+++ my-app/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, FontAwesomeModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('my-app');
+}
---
+++ my-app/src/app/header/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';   
+@Component({
+  selector: 'app-header',
+  imports: [FontAwesomeModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  fasearch = faSearch;
+}
